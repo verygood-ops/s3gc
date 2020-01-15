7b1395317db4d48150724a6ea997fd6f4326dd52
Max Lobur <max_lobur@outlook.com>
2020-01-15 20:31:06 +0200

    Initial

---
+++ s3gc.py
@@ -0,0 +1,63 @@
+import datetime
+import boto3
+import pytz
+import os
+
+s3 = boto3.client('s3')
+S3_BATCH = 1000
+
+BUCKET = os.environ['BUCKET']
+PREFIX = os.environ.get('PREFIX', '')
+DRY_RUN = os.environ.get('DRY_RUN', 'true').lower() in ('true', '1')
+
+NOW = datetime.datetime.now(tz=pytz.UTC)
+DAYS = os.environ.get('DAYS', 0)
+HOURS = os.environ.get('HOURS', 0)
+MINUTES = os.environ.get('MINUTES', 0)
+PAST = NOW - datetime.timedelta(days=DAYS, hours=HOURS, minutes=MINUTES)
+
+
+def main():
+    do_gc(BUCKET, PREFIX, DRY_RUN)
+
+
+def do_gc(bucket, prefix, dry_run):
+    to_delete = []
+    for obj in get_objects_to_delete(bucket, prefix):
+        to_delete.append({'Key': obj['Key']})
+
+        if len(to_delete) == S3_BATCH:
+            delete_objects(bucket, to_delete, dry_run)
+            to_delete.clear()
+
+    delete_objects(bucket, to_delete, dry_run)
+    print('Done.')
+
+
+def get_objects_to_delete(bucket, prefix):
+    kwargs = dict(Bucket=bucket, Prefix=prefix)
+    while True:
+        resp = s3.list_objects_v2(**kwargs)
+        for obj in resp['Contents']:
+            if obj['LastModified'] < PAST:
+                yield obj
+
+        try:
+            kwargs['ContinuationToken'] = resp['NextContinuationToken']
+        except KeyError:
+            break
+
+
+def delete_objects(bucket, objects, dry_run=True):
+    if not objects:
+        print(f"Nothing to delete")
+        return
+    if dry_run:
+        print(f"Dry-run deleting {objects}")
+    else:
+        print(f"Deleting {objects}")
+        s3.delete_objects(Bucket=bucket, Delete={'Objects': objects})
+
+
+if __name__ == "__main__":
+    main()
